ec4873c7c38381521cc0b8b9f3942eac9134d439
Yana <anazigaluk@gmail.com>
2025-04-25 15:38:58 +0300

    node-params-server

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,36 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const fullUrl = `http://${req.headers.host}${req.url}`;
+    const givenUrl = new URL(fullUrl);
+
+    console.log(givenUrl);
+
+    if (givenUrl.pathname.includes('//')) {
+      givenUrl.pathname.replaceAll('//', '/');
+    }
+
+    const response = { parts: [], query: {} };
+
+    response.parts.push(...givenUrl.pathname.slice(1).split('/'));
+
+    const normalizedParams = Object.fromEntries(
+      givenUrl.searchParams.entries(),
+    );
+
+    for (const [key, value] of Object.entries(normalizedParams)) {
+      response.query[key] = value;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(response));
+  });
+
+  return server;
 }
 
 module.exports = {
